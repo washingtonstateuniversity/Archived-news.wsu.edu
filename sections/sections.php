55ad8e6103bd31cd452c85b63996c7858234b507
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-03 12:19:41 -0800

    Add placeholder content for section sides

--- sections/sections.php
+++ sections/sections.php
@@ -24,37 +24,30 @@
 		</ul>
 		<hr>
 		<ul class="crimson-south">
-			<li>Four Great Challenges	
-				<ul>
-					<li>Healthfulness</li>
-					<li>Sustainability</li>
-					<li>Equal Opportunity</li>
-					<li>Infrastructure and Security</li>
-				</ul>
-			</li>
+			<header>Four Great Challenges</header>	
+			<li>Healthfulness</li>
+			<li>Sustainability</li>
+			<li>Opportunity</li>
+			<li>Infrastructure</li>
 		</ul>
 		<hr>
 		<ul class="crimson-south">
-			<li>Popular
-				<ul>
-					<li>Proposed WSU medical college attracts lead gift</li>
-					<li>April 4: Free movie aims to inspire, recruit teachers of color</li>
-				</ul>
-			</li>
+			<header>Popular</header>
+			<li>Proposed WSU medical college attracts lead gift</li>
+			<li>April 4: Free movie aims to inspire, recruit teachers of color</li>
 		</ul>
 		<hr>
 		<ul class="crimson-south">
-			<li>Just In
-				<ul>
-					<li>Proposed WSU medical college attracts lead gift</li>
-					<li>April 4: Free movie aims to inspire, recruit teachers of color</li>
-				</ul>
-			</li>
+			<header>Just In</header>
+			<li>Proposed WSU medical college attracts lead gift</li>
+			<li>April 4: Free movie aims to inspire, recruit teachers of color</li>
 		</ul>
 		
 		</aside>
 		
-		<div class="articles column one">
+		<div class="articles lead column one">
+	
+	<div class="article-featured">
 	
 	<?php
 	
@@ -76,49 +69,64 @@
 		
 	 ?>
 	 
-	 <section id="" class="today-tomorrow">
-		 
-		 <header>Today</header>
-		 
-		 <a href="#">
-		 <dl class="cf">
-			 <dt>
-			 	Washington Policy Center’s Young Professionals minimum wage debate
-			 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
-			 </dt>
-			 <dd>
-			 	<time><span class="hour">8:45</span><span class="oclock">P.M.</span></time>
-			 	
-			 </dd>
-		 </dl>
-		 </a>
-		 <a href="#">
-		 <dl class="cf">
-			 <dt>
-			 	<span class="series">Lunch & Learn</span>
-			 	<span class="title">Interviewing Basics for New Healthcare Professionals</span>
-			 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
-			 </dt>
-			 <dd>
-			 	<time><span class="hour">6:00</span><span class="oclock">P.M.</span></time>
-			 	
-			 </dd>
-		 </dl>
-		 </a>
-		 <a href="#">
-		 <dl class="cf">
-			 <dt>
-			 	<a href="">Washington Policy Center’s Young Professionals minimum wage debate</a>
-			 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
-			 </dt>
-			 <dd>
-			 	<time><span class="hour">2:30</span><span class="oclock">P.M.</span></time>
-			 	
-			 </dd>
-		 </dl>
-		 </a>
+	</div>
 	 
-	 </section>
+	 <div id="" class="today-tomorrow">
+		 
+		 <dl class="tab">
+				 	<dt class="today" tab="1">Today</dt>
+				 	<dt class="tomorrow" tab="2">Tomorrow</dt>
+				 	<dd class="today">
+		 
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="event-title">Washington Policy Center’s Young Professionals minimum wage debate</span>
+						 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">8:45</span><span class="meridiem">AM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="series">Lunch & Learn</span>
+						 	<span class="event-title">Interviewing Basics for New Healthcare Professionals</span>
+						 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">6:00</span><span class="meridiem">PM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="event-title">Resource Nights: “Capital Ideas”</span>
+						 	<address>Todd Addition, Room 268, Pullman</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">8:30</span><span class="meridiem">PM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+		 
+				</dd>
+				
+				<dd class="tomorrow" tab="2">
+				</dd>
+
+		</dl>
+	 
+	 </div>
+	 
+	 <div class="articles-continued">
+		 <header>Headlines</header>
 	 
 	 <?php
 	
@@ -140,6 +148,8 @@
 		wp_reset_postdata();
 		
 	 ?>
+	 
+	 </div>
 	 
 	 
 		</div><!--/ articles -->
