60c6c82f3f31c8c33c684bf531ad33b6a6e2dc8c
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-11 12:44:18 -0700

    Move today/tomorrow lorem ipsum to separate file

---
+++ sections/today.php
@@ -0,0 +1,53 @@
+<div id="" class="today-tomorrow">
+		 
+		 <dl class="tab">
+				 	<dt class="today" tab="1">Today</dt>
+				 	<dt class="tomorrow" tab="2">Tomorrow</dt>
+				 	<dd class="today">
+		 
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="event-title">Washington Policy Center’s Young Professionals minimum wage debate</span>
+						 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">8:45</span><span class="meridiem">AM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="series">Lunch & Learn</span>
+						 	<span class="event-title">Interviewing Basics for New Healthcare Professionals</span>
+						 	<address>Bryan Hall Theatre, WSU Campus, Pullman, WA</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">6:00</span><span class="meridiem">PM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+					 <a href="#">
+					 <dl class="event cf">
+						 <dt>
+						 	<span class="event-title">Resource Nights: “Capital Ideas”</span>
+						 	<address>Todd Addition, Room 268, Pullman</address>
+						 </dt>
+						 <dd>
+						 	<time><span class="hour">8:30</span><span class="meridiem">PM</span></time>
+						 	
+						 </dd>
+					 </dl>
+					 </a>
+		 
+				</dd>
+				
+				<dd class="tomorrow" tab="2">
+				</dd>
+
+		</dl>
+	 
+	 </div>
