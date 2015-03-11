a17decaf8fd48a635f3ec2cd1bd7a11d6aad5623
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-10 22:38:18 -0700

    Add admin js for news themes

---
+++ includes/theme-admin.js
@@ -0,0 +1,29 @@
+
+
+
+
+(function($){
+
+
+	$(document).mouseup(function() {
+		
+		
+		
+	if ($('body.modal-open:not(modal-tweaked)').length) {
+		// alert("hello");
+		
+		$('.gallery-settings select.link-to').val('none');
+		$('.gallery-settings select.size').val('large');
+		
+		/*$('body:not(.modal-tweaked) .gallery-settings').append('<label><span class="name">Caption</span><textarea id="gallery-caption" name="style"></textarea></label>'); */
+		
+		
+		$('body').addClass('modal-tweaked');
+		
+		}
+
+
+
+	}); 
+
+}(jQuery));
