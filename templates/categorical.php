ed458a69510588bd9873e3e02ccc7d08dd1e3fa3
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-11 12:45:38 -0700

    Move categorical functionality into sections template

--- templates/categorical.php
+++ templates/categorical.php
@@ -10,19 +10,22 @@
 
 <main class="<?php // echo $categories; ?>">
 
-<?php get_template_part('parts/headers'); ?>
-<?php get_template_part('parts/featured-images'); ?>
+<?php get_template_part( 'sections/sections' ); ?>
 
 <section class="row margin-right gutter pad-ends">
 
 	<div class="column one">
 		
-		<?php 
+		<?php $section = get_section(); ?>
+		
+		<?php
+			
+			$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
 		
 			$articles_filter = array(
 				'nopaging'			=> false,
-				'paged'				=> '1',
-				'posts_per_page'	=> 20,
+				'paged'				=> $paged,
+				'posts_per_page'	=> 10,
 				'cat'				=> $categories,
 				'post_status'		=> 'publish',
 			);
@@ -49,15 +52,42 @@
 </section>
 
 <footer>
-	<?php
+		
+	<?php //get_template_part( 'parts/pager', '' ); ?>
+		
+<?php
 
+$big = 999999999; // need an unlikely integer
+
+$paging = array(
+	'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format'       => '/page/%#%',
+	'total'        => $articles->max_num_pages,
+	'current'      => max( 1, get_query_var('paged') ),
+	'show_all'     => False,
+	'end_size'     => 3,
+	'mid_size'     => 3,
+	'prev_next'    => True,
+	'prev_text'    => __('« '),
+	'next_text'    => __(' »'),
+	'type'         => 'plain',
+	'add_args'     => False,
+	'add_fragment' => ''
+); ?>
+
+<nav class="paging">
+	<?php echo paginate_links( $paging ); ?>
+</nav>
+	
+
+<?php
 // next_posts_link() usage with max_num_pages
 next_posts_link( 'Older Entries', $articles->max_num_pages );
 previous_posts_link( 'Newer Entries' );
 ?>
 </footer>
 
-<?php get_template_part( 'sections/sections' ); ?>
+
 
 
 </main>
