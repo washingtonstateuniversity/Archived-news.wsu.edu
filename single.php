3612711f675f76856038503aa5be6cf202f4d6dc
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-20 16:48:06 -0700

    Use common sections.php for single footer

--- single.php
+++ single.php
@@ -39,50 +39,7 @@ if ( spine_has_background_image() ) {
 </section>
 
 <footer class="main-footer">
-	
-	<section class="row single pad wide">
-	
-		<div class="column one">
-			
-		<ul class="footer-sections padless">
-			<li class="news crimson"><a href="">Topics</a></li>
-			<li class="local yellow">
-				<a href="">Local</a>
-				<ul>
-					<li><a href="">Pullman</a></li>
-					<li><a href="">Vancouver</a></li>
-					<li><a href="">Spokane</a></li>
-					<li><a href="">Tri-Cities</a></li>
-					<li><a href="">Global Campus</a></li>
-				</ul>
-			</li>
-			<li class="press blue">
-				<a href="">Press</a>
-				<ul>
-					<li><a href="">Press Releases</a></li>
-					<li><a href="">In the News</a></li>
-				</ul>
-			</li>
-			<li class="events orange">
-				<a href="">Events</a>
-				<ul>
-					<li><a href="">Resource Nights: "Capital Ideas"</a></li>
-					<li><a href="">Graduate research Seminar Presentation</a></li>
-					<li><a href="">Student Recital: Kathy Perng, violin</a></li>
-				</ul>
-			</li>
-			<li class="staff green">
-				<a href="">Staff</a>
-				<ul>
-					<li><a href="">Announcements</a></li>
-				</ul>
-			</li>
-		</ul>
 		
-		</div><!--/column one-->
-	
-	</section>
-	
 	<section class="row halves pager prevnext gutter pad-ends">
 		<div class="column one">
 			<?php previous_post_link(); ?>
@@ -91,6 +48,8 @@ if ( spine_has_background_image() ) {
 			<?php next_post_link(); ?>
 		</div>
 	</section><!--pager-->
+	
+	<?php include(locate_template('sections/sections.php')); ?>
 
 </footer>
 
