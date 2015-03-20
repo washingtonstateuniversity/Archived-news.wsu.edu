991f85dacb9cd45e2e5f13fab1e9a0e661f5dfbd
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-19 17:03:48 -0700

    Now in widget area with json pull

--- sections/side-events.php
+++ sections/side-events.php
@@ -1,10 +1,1 @@
-		<ul>
-			<li><a href="">WSU In the Media</a></li>
-			<li><a href="">Calendar</a></li>
-			
-			<hr>
-			
-			<li><a href="">Submit Event</a></li>
-			
-			
-		</ul>
+calendar here
