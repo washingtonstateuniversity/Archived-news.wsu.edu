de23510e581b5a6e75a70fdd99994ce240054bf8
Nathan Jacobson <nate.jacobson@wsu.edu>
2015-03-31 17:06:47 -0700

    Temporarily add search-opened until it's available in spine parent

---
+++ spine/body.php
@@ -0,0 +1,27 @@
+<?php
+
+if ( true == spine_get_option( 'crop' ) && is_front_page() ) {
+	$cropping = ' cropped';
+} else {
+	$cropping = '';
+}
+
+?>
+
+<div id="spine" class="spine-column search-opened <?php echo esc_attr( spine_get_option( 'spine_color' ) ); echo $cropping; echo esc_attr( spine_get_option( 'bleed' ) ); ?> shelved">
+<div id="glue" class="spine-glue">
+
+<?php get_template_part( 'spine/header' ); ?>
+
+	<section id="spine-navigation" class="spine-navigation">
+
+		<?php get_template_part( 'spine/site-navigation' ); ?>
+
+		<?php get_template_part( 'spine/offsite-navigation' ); ?>
+
+	</section>
+
+<?php get_template_part( 'spine/footer' ); ?>
+
+</div><!--/glue-->
+</div><!--/spine-->
